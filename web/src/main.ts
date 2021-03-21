452828e597ca18d4d67b345e93708c287eafb64b
lin-716 <lin1997716@gmail.com>
2021-03-20 20:10:26 -0400

    axios interceptor

--- web/src/main.ts
+++ web/src/main.ts
@@ -22,3 +22,19 @@ for (const i in icons) {
 //test multi-environment
 console.log('environment:client', process.env.NODE_ENV)
 console.log('environment:server', process.env.VUE_APP_SERVER)
+
+//axios拦截器
+//拦截请求
+axios.interceptors.request.use(function(config){
+    console.log('request param:' + config)
+    return config
+},error => {
+    return Promise.reject(error)
+})
+//拦截返回
+axios.interceptors.response.use(function(response){
+    console.log('response' + response)
+    return response
+},error => {
+    return Promise.reject(error)
+})
