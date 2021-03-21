3989f3b89dae3715a671de2d7aacf789393dc6f7
lin-716 <lin1997716@gmail.com>
2021-03-21 13:08:59 -0400

    admin-book view

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="admin-ebook">
-    <h1>ebook management</h1>
-  </div>
+
+  <a-layout class="home">
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="admin-ebook">
+        <h1>ebook management</h1>
+      </div>
+     </a-layout-content>
+  </a-layout>
 </template>
