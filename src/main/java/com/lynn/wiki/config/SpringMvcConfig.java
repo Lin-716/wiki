e639896d5026b957614b12006e7fac21d71fd2ed
lin-716 <lin1997716@gmail.com>
2021-03-20 21:07:39 -0400

    aop,打印接口耗时，请求参数和返回参数 删除拦截器和过滤器

--- src/main/java/com/lynn/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/lynn/wiki/config/SpringMvcConfig.java
@@ -1,22 +1,22 @@
-package com.lynn.wiki.config;
-
-import com.lynn.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    //注入拦截器
-    @Resource
-    LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-        //增加所有请求再去掉某些请求
-    }
-}
+//package com.lynn.wiki.config;
+//
+//import com.lynn.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    //注入拦截器
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry){
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//        //增加所有请求再去掉某些请求
+//    }
+//}
