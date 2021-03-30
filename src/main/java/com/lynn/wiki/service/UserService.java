e106710eab7068d1590b48c23a188945411efd41
lin-716 <lin1997716@gmail.com>
2021-03-29 20:06:43 -0400

    用户名不重复校验，和编辑用户名不能修改

--- src/main/java/com/lynn/wiki/service/UserService.java
+++ src/main/java/com/lynn/wiki/service/UserService.java
@@ -72,6 +72,7 @@ public class UserService {
             }
         }else{
             //更新
+            user.setLoginName(null);
             userMapper.updateByPrimaryKeySelective(user);//Selective表示user有值才去更新
         }
     }
