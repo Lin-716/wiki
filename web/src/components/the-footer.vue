03ad1c3a29b3f054772e0113cbc61630dac8314a
lin-716 <lin1997716@gmail.com>
2021-03-30 18:59:34 -0400

    使用vuex全局获取登录信息

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,12 +1,19 @@
 <template>
   <a-layout-footer style="text-align: center">
-    Ant Design ©2018 Created by Ant UED
+    {{user.name}}
   </a-layout-footer>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed } from 'vue';
+import store from "@/store";
 
 export default defineComponent({
-  name: 'the-footer'
+  name: 'the-footer',
+  setup() {
+    const user = computed(() => store.state.user)
+    return {
+      user
+    }
+  }
 });
 </script>
--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,12 +1,19 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+//定义全局变量,操作在mutations和actions，mutations同步，actions异步
+const store = createStore({
   state: {
+    user:{ }
   },
   mutations: {
+    //state是全局的，右边的user是外部传进来的变量不是state里的
+    setUser (state,user){
+      state.user = user
+    }
   },
   actions: {
   },
   modules: {
   }
 })
+export default store
