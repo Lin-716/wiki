d0bdf8b054e5efaa18d751b2ea097c5f4c81ec3c
lin-716 <lin1997716@gmail.com>
2021-03-30 18:44:54 -0400

    前端显示登录昵称，刷新后无了

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -22,7 +22,10 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" @click="showLoginModal">
+      <a class="login-menu" v-show="user.id">
+        <span>hi, {{ user.name }}</span>
+      </a>
+      <a class="login-menu" @click="showLoginModal" v-show="!user.id">
         <span>登录</span>
       </a>
     </a-menu>
@@ -55,6 +58,10 @@ declare let KEY:any
 export default defineComponent({
   name: 'the-header',
   setup(){
+    //登录后保存
+    const user = ref()
+    user.value=[]//避免空指针异常
+    //登录
     const loginUser = ref({
       loginName:"test",
       password:"test"
@@ -76,6 +83,7 @@ export default defineComponent({
         if(data.success){
           loginModalVisible.value = false
           message.success("登录成功")
+          user.value = data.content
         }else{
           message.error("登录失败")
         }
@@ -87,7 +95,8 @@ export default defineComponent({
       loginModalLoading,
       loginModalVisible,
       login,
-      showLoginModal
+      showLoginModal,
+      user
     }
 
   }
